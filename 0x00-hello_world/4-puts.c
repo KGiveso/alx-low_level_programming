d2ac9ea5bd3472b66128486c88f2f068a69a448f
KGiveso <mogoboyagiven@gmail.com>
2023-02-16 17:29:30 -0800

    Added All Multiple files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * return: always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multingual puzzle");
+return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This chapter i will be writing my first C programming language using Betty and C to execute Hello World
