8f5b019e1d8bbbc5dfe8cc9080ff2494f89aa54c
KGIveso <mogoboyagiven@gmail.com>
2023-06-08 17:35:18 -0700

    mandatory expt 6

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, j = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (j == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			j = 1;
+		}
+	}
+	printf("}\n");
+}
